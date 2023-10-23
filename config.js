eb8cf4dcd1b8598dae3d16e999389e59816b3a17
Michal Ferák <michal.ferak@me.com>
2023-10-23 17:27:45 +0200

    Babel plugins

--- config.js
+++ config.js
@@ -295,10 +295,7 @@ const scripts = ({
         ],
       ],
       plugins: [
-        ['@babel/plugin-proposal-class-properties', { loose: false }],
-        ['@babel/plugin-proposal-object-rest-spread', { loose: false }],
-        ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
-        ['@babel/plugin-proposal-private-methods', { loose: false }],
+        ['transform-object-rest-spread', { loose: false }],
       ],
     },
     context: cwd,
